a4a316d0d156d5283dd3c96a64c3cb353d1c8778
= <=>
2019-04-17 11:54:37 -0300

    Finalizar aula

--- aula02-03/script.js
+++ aula02-03/script.js
@@ -1,6 +1,56 @@
-let filmes = ['Harry Potter', 'Titanic', 'Laranja Mecânica', 'Wall-e'];
-let i;
+// let filmes = ['Harry Potter', 'Titanic', 'Laranja Mecânica', 'Wall-e'];
+// let i;
 
-for (i=0; i<filmes.length; i++) {
-    console.log(i + ". " + filmes[i]);
+// // for tradicional
+// for (i=0; i<filmes.length; i++) {
+//     console.log(i + ". " + filmes[i]);
+// }
+
+// // for of = percorre os elementos da lista
+// for (let item of filmes) {
+//     console.log(item);
+// }
+
+// // for in = percorre indices
+// for (let item in filmes) {
+//     console.log(filmes[item]);
+// }
+
+// // calculadora
+// function calculadora(a,b) {
+
+// }
+// let continuar = true;
+// while (cons)
+// console.log(calculaMedia(lista));
+// // media
+// function calculaMedia(lista) {
+//     let soma = 0, cont = 0, media = 0;
+//     for (let item in lista) {
+//         soma += lista[item];
+//         cont = cont + 1;
+//     }
+//     media = soma/cont;
+//     return media;
+// }
+
+// // arrow function 
+// calcMedia = (n1,n2,n3) => {
+//     return ((n1+n2+n3)/3).toFixed(1);
+// }
+var idade = prompt("Digite sua idade: ");
+indicaFilme(idade);
+function indicaFilme(idade) {
+    let filmes = [
+        {titulo:"Star Wars", classificacao: 14},
+        {titulo:"Shrek", classificacao: 0},
+        {titulo:"Ninja Assassino", classificacao: 18},
+    ];
+    let text = "";
+    for (let item of filmes) {
+        if (item.classificacao <= idade) {
+            text += "\n" + item.titulo;
+        }
+    }
+    alert("Filmes Indicados: " + text);
 }
