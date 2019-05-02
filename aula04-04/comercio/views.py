3fd19e51b27f95010aadf6741c64f395e842edfa
Nathalia <nathaliaspinula@gmail.com>
2019-05-02 11:42:47 -0300

    a

--- aula04-04/comercio/views.py
+++ aula04-04/comercio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from comercio.forms import PedidoForm
 # Create your views here.
 def fazer_pedido(request):
+    msg = ''
     formulario = PedidoForm(request.POST or None)
     if formulario.is_valid(): 
         formulario.save()
