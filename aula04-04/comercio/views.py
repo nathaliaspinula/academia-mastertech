bc967e825f50127a2e94eb9fe60f16d60deecca9
Nathalia <nathaliaspinula@gmail.com>
2019-05-02 11:25:27 -0300

    Finalizar aula04-04

--- aula04-04/comercio/views.py
+++ aula04-04/comercio/views.py
@@ -3,13 +3,10 @@ from comercio.forms import PedidoForm
 # Create your views here.
 def fazer_pedido(request):
     formulario = PedidoForm(request.POST or None)
-    msg = ''
     if formulario.is_valid(): 
         formulario.save()
         formulario = PedidoForm()
         msg = 'Pedido realizado com sucesso'
-    else: 
-        msg = 'Error'
     
     content = {
         'form' : formulario,
